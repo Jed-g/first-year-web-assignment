b513213729add61402b8d9d0a83ef42c6f6f9ea2
Jed G <jed.golebiewski@gmail.com>
2020-10-21 23:17:10 +0100

    Add main menu animation + logo redirect to index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-let date = new Date();
+const date = new Date();
 
 document.querySelector(
   "#copyright"
 ).textContent = `© Jedrzej Golebiewski ${date.getFullYear()}`;
+
+document.querySelector("#logo").addEventListener("click", redirectToIndexHtml);
+
+const menuButton = document.querySelector("#menu-icon");
+menuButton.addEventListener("click", animateMenuIconClick);
+
+function redirectToIndexHtml() {
+    window.location.href = "./index.html"
+}
+
+function animateMenuIconClick() {
+  this.classList.toggle("animate-menu-icon-full");
+  if (this.classList.contains("animate-menu-icon-full")) {
+    document.querySelector("#side-menu").style.height = "100vh";
+    document.querySelector("#logo").style.opacity = 0;
+    document.querySelector("#current-page-info").style.opacity = 0;
+    document.querySelector("header").style.backgroundColor = "#EB5E28";
+    document.querySelector("#logo").removeEventListener("click", redirectToIndexHtml);
+  } else {
+    document.querySelector("#logo").addEventListener("click", redirectToIndexHtml);
+    document.querySelector("#side-menu").style.height = 0;
+    document.querySelector("#logo").style.opacity = "100%";
+    document.querySelector("#current-page-info").style.opacity = "100%";
+    document.querySelector("header").style.backgroundColor = "#252422";
+  }
+}
