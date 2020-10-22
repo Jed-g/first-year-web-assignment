eaf3597ff94fb1eca69add1fc103a5ea95ac5ce7
Jed G <jed.golebiewski@gmail.com>
2020-10-22 13:47:37 +0100

    Add menu links animation

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ document.querySelector(
 
 document.querySelector("#logo").addEventListener("click", redirectToIndexHtml);
 
+function redirectToIndexHtml() {
+  window.location.href = "./index.html";
+}
+
 const menuButton = document.querySelector("#menu-icon");
 menuButton.addEventListener("click", animateMenuIconClick);
 
-function redirectToIndexHtml() {
-    window.location.href = "./index.html"
-}
+const menuLinkArray = document.querySelectorAll("#anchor-tags a");
 
 function animateMenuIconClick() {
   this.classList.toggle("animate-menu-icon-full");
@@ -20,12 +22,23 @@ function animateMenuIconClick() {
     document.querySelector("#logo").style.opacity = 0;
     document.querySelector("#current-page-info").style.opacity = 0;
     document.querySelector("header").style.backgroundColor = "#EB5E28";
-    document.querySelector("#logo").removeEventListener("click", redirectToIndexHtml);
+    document
+      .querySelector("#logo")
+      .removeEventListener("click", redirectToIndexHtml);
+
+      for (let i = 0; i < menuLinkArray.length; i++){
+        setTimeout(() => menuLinkArray[i].style.marginLeft = "8%", i * 80);
+      }
   } else {
-    document.querySelector("#logo").addEventListener("click", redirectToIndexHtml);
+    document
+      .querySelector("#logo")
+      .addEventListener("click", redirectToIndexHtml);
     document.querySelector("#side-menu").style.height = 0;
     document.querySelector("#logo").style.opacity = "100%";
     document.querySelector("#current-page-info").style.opacity = "100%";
     document.querySelector("header").style.backgroundColor = "#252422";
+    for (let i = 0; i < menuLinkArray.length; i++){
+      setTimeout(() => menuLinkArray[i].style.marginLeft = "-200%", i * 80);
+    }
   }
 }
